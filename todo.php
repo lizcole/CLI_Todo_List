d32bd70652791d5ea3abdc0d6c0e41d15ca47e0c
Liz Cole <liz.cole1227@gmail.com>
2014-10-07 15:22:33 -0500

    added notes and exercise for to-do list

---
+++ todo.php
@@ -0,0 +1,30 @@
+<?php
+
+$items = array();
+
+//$key is equal to [0] [1] [2] [3] etc. thus you need to increment it by one to get it to start at 1 and then you need to decrement it by 1 for deleting the list item, so the cl reads the correct array item to remove.
+do {
+    foreach ($items as $key => $item) {
+        $key++;
+        echo "[{$key}] {$item}\n";
+    }
+
+    echo '(N)ew item, (R)emove item, (Q)uit : ';
+
+    $input = strtoupper(trim(fgets(STDIN)));
+
+    if ($input == 'N' ) {
+        echo 'Enter item: ';
+        $items[] = trim(fgets(STDIN));
+        
+    } elseif ($input == 'R') {
+        echo 'Enter item number to remove: ';
+        $key = trim(fgets(STDIN));
+        $key--;
+        unset($items[$key]);
+    }
+} while ($input != 'Q');
+
+echo "Goodbye!\n";
+
+exit(0);
---
+++ todo_notes.php
@@ -0,0 +1,46 @@
+<?php
+
+<?php
+
+// Create array to hold list of todo items
+$items = array();
+
+// The loop!
+do {
+    // Iterate through list items
+    foreach ($items as $key => $item) {
+        // Display each item and a newline
+        echo "[{$key}] {$item}\n";
+    }
+    
+    // Show the menu options
+    echo '(N)ew item, (R)emove item, (Q)uit : ';
+
+    // Get the input from user
+    // Use trim() to remove whitespace and newlines
+    $input = trim(fgets(STDIN));
+
+    // Check for actionable input
+    if ($input == 'N') {
+        // Ask for entry
+        echo 'Enter item: ';
+        // Add entry to list array
+        $items[] = trim(fgets(STDIN)); // [] - allows user input to become apart of
+        // the array defined, it always adds to the end of the array (only used for arrays)
+    } elseif ($input == 'R') {
+        // Remove which item?
+        echo 'Enter item number to remove: ';
+        // Get array key
+        $key = trim(fgets(STDIN));
+        // Remove from array
+        unset($items[$key]);// UNSET is a given variable and is used to unset a variable 
+        //or an array
+    }
+// Exit when input is (Q)uit
+} while ($input != 'Q');
+
+// Say Goodbye!
+echo "Goodbye!\n";
+
+// Exit with 0 errors
+exit(0);
