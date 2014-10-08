5e6a08ff317ccf5369700c55350be6cdcc101869
Liz Cole <liz.cole1227@gmail.com>
2014-10-08 16:05:24 -0500

    added functions to todo list

--- todo.php
+++ todo.php
@@ -2,24 +2,37 @@
 
 $items = array();
 
-//$key is equal to [0] [1] [2] [3] etc. thus you need to increment it by one(++) to get it to start at '[1]' and then you need to decrement it by one(--) for deleting the list item, so the cl reads the correct array item to remove.
-do {
-    foreach ($items as $key => $item) {
+
+
+function listItems($list){
+    $string = '';
+    foreach ($list as $key => $item) {
         $key++;
-        echo "[{$key}] {$item}\n";
+        $string .= "[{$key}] {$item}" . PHP_EOL; 
     }
 
+    return $string;
+}
+
+function getInput($upper = false) {
+     
+     return trim(fgets(STDIN));
+}
+
+do {
+    echo listItems($items); 
+ 
     echo '(N)ew item, (R)emove item, (Q)uit : ';
 
     $input = strtoupper(trim(fgets(STDIN)));
-
+ 
     if ($input == 'N' ) {
         echo 'Enter item: ';
-        $items[] = trim(fgets(STDIN));
+        $items[] = getInput($input);
         
     } elseif ($input == 'R') {
         echo 'Enter item number to remove: ';
-        $key = trim(fgets(STDIN));
+        $key = getInput($input);
         $key--;
         unset($items[$key]);
     }
